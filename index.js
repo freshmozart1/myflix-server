99163eff432493078b5752f3ad63bf9d94c42610
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-06-29 18:25:11 +0200

    I added the route /movies?limit to the index.js and modified movies.json. Movies.json now is a list of 10 different popular movie titles.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express'),
     app = express(),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
+    fs = require('fs');
 
 app.use(morgan('common'));
 app.use(express.static(__dirname));
@@ -11,8 +12,21 @@ app.get('/', (req, res) => {
     res.send('hello, world');
 });
 
-app.get('/movies', (req, res) => {
-    res.sendFile('movies.json', { root: __dirname });
+app.get('/movies?:limit', (req, res) => {
+    fs.readFile(__dirname + '/movies.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let movies = JSON.parse(data)['movies'];
+        if (req.query['limit']) {
+            const limit = Number(req.query['limit']);
+            if (isNaN(limit)) throw new Error('Invalid limit');
+            if (limit > 0) {
+                movies = movies.slice(0, limit);
+            }
+            res.json({ "movies": movies });
+        } else {
+            res.json({ "movies": movies });
+        }
+    });
 });
 
 app.use(bodyParser.urlencoded({
