fc82d3196d646ab17b6212ee857277d7afc515c4
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-11 14:24:07 +0200

    If someone provides wrong password or username, the response should end with a error object.

--- auth.js
+++ auth.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
-    
+
 require('./passport.js');
 
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', {session: false}, (error, user) => {
+        passport.authenticate('local', { session: false }, (error, user) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Please provide a valid username and password.',
                     user: user
                 }).end();
             }
-            req.login(user, {session: false}, error => {
+            req.login(user, { session: false }, error => {
                 if (error) {
-                    res.send(error);
+                    res.end(error);
                 } else {
                     const token = (user => {
                         return jwt.sign(user, process.env.JWT_SECRET, {
@@ -23,7 +23,7 @@ module.exports = (router) => {
                             algorithm: 'HS256'
                         });
                     })(user.toJSON());
-                    return res.json({user, token}).end();
+                    return res.json({ user, token }).end();
                 }
             });
         })(req, res);
